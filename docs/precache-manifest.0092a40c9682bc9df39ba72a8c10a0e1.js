2e5f6555d5143c2438c7bf02e1db0e0160c985d2
dmorad1004 <dmorad@unal.edu.co>
2020-10-16 19:28:21 -0500

    docs agregadi

---
+++ docs/precache-manifest.0092a40c9682bc9df39ba72a8c10a0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422a52c0610437910698327e1a9436c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56018ec1c65f035cd078",
+    "url": "/static/css/main.fcb90b87.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56018ec1c65f035cd078",
+    "url": "/static/js/main.f06fcfe3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
